ba7637dc9ccbc67de4a35a2e11077adac6cb36bc
Pierre-Jean Fichet <pierrejean.fichet@posteo.net>
2020-12-24 17:35:31 +0100

    Incrémente le numéro de version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name = "tplan",
 	description = "Calculates business plans",
-	version = "0.1",
+	version = "0.2",
 	url = None,
 	author = "Pierre-Jean Fichet",
 	author_email = None,
